a965eb1aed075bf2078c5a679982b1dbea3cc460
stupidcomputer <ryan@beepboop.systems>
2024-06-29 18:15:33 -0500

    add a homepage thing

--- gen_kb.sh
+++ gen_kb.sh
@@ -2,9 +2,12 @@ files=$(find kb -type f | awk -F'/' '{print $NF}')
 IFS='
 '
 mkdir -p franklincce/staticfiles/kb
+mkdir -p franklincce/staticfiles/root
 
 for file in $files; do
   	without_extension=${file%.*}
     echo $file, $without_extension
     pandoc -s --template=./template.html -f markdown -t html -o "franklincce/staticfiles/kb/$without_extension.html" "kb/$without_extension.md" --lua-filter=links-to-html.lua
 done
+
+cp franklincce/staticfiles/kb/web_root.html franklincce/staticfiles/root/index.html
---
+++ kb/web_root.md
@@ -0,0 +1,6 @@
+---
+title: "Franklin CCE Delegation website"
+---
+
+This website contains some materials for the Franklin High School YMCA Center for Civic Engagement delegation.
+Specifically, a [bill database](/explorer) and a [collection of helpful articles](/kb) on various things are available here.
